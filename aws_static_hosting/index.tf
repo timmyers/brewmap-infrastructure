15b4e2edb9bc35a1a279c58dabeb7b12e8a3ed0b
Tim Myers <tmyers@closetbox.me>
2017-11-16 20:50:54 -0700

    s3 permissions

--- aws_static_hosting/index.tf
+++ aws_static_hosting/index.tf
@@ -10,6 +10,21 @@ resource "aws_s3_bucket" "s3" {
     index_document = "index.html"
     error_document = "index.html"
   }
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement":[
+    {
+      "Sid":"AllowPublicRead",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::www.brewmap.co/*"]
+    }
+  ]
+}
+POLICY
 }
 
 resource "aws_s3_bucket" "logs" {
